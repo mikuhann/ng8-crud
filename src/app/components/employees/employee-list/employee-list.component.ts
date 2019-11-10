c7d72de6ccdfbc3fa89064684bee633927bc8101
Mikuhann <ng.kano@gmail.com>
2019-11-10 21:38:51 +0300

    add delete employee

--- src/app/components/employees/employee-list/employee-list.component.ts
+++ src/app/components/employees/employee-list/employee-list.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { EmployeeService } from '../../../services/employee.service';
+import { ToastrService } from 'ngx-toastr';
+
 import { Employee } from '../../../models/employee.model';
+
+import { employees } from '../../../constants/constants';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,7 +18,9 @@ export class EmployeeListComponent implements OnInit {
   list: Employee[];
 
   constructor(
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
@@ -28,5 +35,11 @@ export class EmployeeListComponent implements OnInit {
   }
   onEdit(employee: Employee) {
     this.employeeService.formData = Object.assign({}, employee);
+  }
+  onDelete(id: string) {
+    if (confirm('Are you sure want to delete this record')) {
+      this.firestore.doc(`${employees}/${id}`).delete();
+      this.toastr.warning('Employee has been deleted');
+    }
   }
 }
